3d52d9f07d05544ca399eec26fdb6aa26286e2b6
saintson1 <pan.alexander2006@gmail.com>
2025-03-17 14:23:05 +0300

    Add unrolled_list_store.

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <iterator>
 
-#include <unrolled_list.h>
+#include <unrolled_list.hpp>
 
 int main(int argc, char** argv) {
-    std::cout << "Hello, world!" << std::endl;
+    // int a;
+    // std::cout << typeid(decltype((labwork7::details::Iterator<labwork7::unrolled_list<int>>::reference(a)))).name();
+
+    // static_assert(std::bidirectional_iterator<labwork7::details::Iterator<labwork7::unrolled_list<int>>>, "alblack");
+    labwork7::unrolled_list<int> cont;
+    labwork7::unrolled_list<int>::const_iterator c_itr;
+    labwork7::unrolled_list<int>::iterator itr{c_itr};
+
+    cont.insert(cont.cend(), 7);
+
+    // labwork7::unrolled_list<int, std::allocator<int>, false> scd;
+    // fst.func();
+    // std::cout << "\n";
+    // scd.func();
+
     return 0;
 }
